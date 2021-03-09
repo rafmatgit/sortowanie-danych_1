873b92522e01353503d13a07f5aa43c4a088fb5f
DonRafal <rafalmatusik1@gmail.com>
2021-03-09 19:57:33 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const btn = document.querySelector("a");
 const time = document.querySelectorAll("tbody tr td");
+const arrow = document.querySelector("i");
 const arr = [];
 
-time.forEach((e, i) => {
+time.forEach(e => {
   if (parseInt(e.innerHTML)) {
-    arr.push(
-      new Date(parseInt(e.innerHTML) * 1000).toISOString().substr(11, 8)
-    );
+    arr.push(parseInt(e.innerHTML));
   }
 });
-const one = arr[0];
-let two = arr[1];
-let three = arr[2];
 
-btn.addEventListener("click", () => {
+function sortTableFromLowest() {
+  arr.sort((a, b) => {
+    return a - b;
+  });
+}
+
+function sortTableFromHighest() {
+  arr.sort((a, b) => {
+    return b - a;
+  });
+}
+
+function convertToHours() {
+  new Date(parseInt(e.innerHTML) * 1000).toISOString().substr(11, 8);
+}
+function printTime() {
   let i = 0;
   time.forEach(e => {
     if (parseInt(e.innerHTML)) {
-      console.log(e.innerHTML, i);
       e.innerHTML = arr[i];
       i++;
     }
   });
-});
+}
+
+function changeArrowDirection() {
+  if (arr[0] > arr[2]) {
+    arrow.classList = "fas fa-caret-down";
+  } else {
+    arrow.classList = "fas fa-caret-up";
+  }
+}
+
+function changeTableOrder() {}
